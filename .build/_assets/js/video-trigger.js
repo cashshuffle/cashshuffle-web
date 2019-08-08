226d46c756d75366052e32712c3f588800f93264
Cameron McEwan <cameron@solomonpierce.com>
2019-08-08 14:13:35 +0900

    Build results from final tweaks

--- .build/_assets/js/video-trigger.js
+++ .build/_assets/js/video-trigger.js
@@ -1,3 +1,3 @@
 window.addEventListener('DOMContentLoaded', function () {
-    new ModalVideo("#modal-video", { channel: 'youtube' });
+  new ModalVideo("#modal-video", { channel: 'youtube' });
 });
